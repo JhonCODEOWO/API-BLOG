3fbf678ad32c1fecfb264e5d851bd21a49b09c2f
JhonCode <jjv20618@gmail.com>
2024-12-19 16:10:35 -0600

    Connection settings added and a new resource for users.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    config
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '3306',
+      username: 'root',
+      password: '07092002fake',
+      database: 'BlogJhonDev',
+      entities: [],
+      synchronize: false,
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
